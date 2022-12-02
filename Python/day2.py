4645721cf98dd85ee9736a05cbd69f1bef6c3e68
Sai Nishwanth Raj <sainishwanthraj@gmail.com>
2022-12-02 16:59:45 +0530

    Python Day-2

--- Python/day2.py
+++ Python/day2.py
@@ -2,7 +2,7 @@ with open('day2.txt', "r") as file:
     content = file.read().split("\n")
 
 score = 0
-
+score2 = 0
 for i in content:
     try:
         p1 = i[0]
@@ -12,56 +12,34 @@ for i in content:
     if p1 == 'A':
         if p2 == 'X':
             score += 1 + 3
+            score2 += 0 + 3
         elif p2 == 'Y':
             score += 2 + 6
+            score2 += 3 + 1
         elif p2 == 'Z':
             score += 3 + 0
+            score2 += 6 + 2
     elif p1 == 'B':
         if p2 == 'X':
             score += 1 + 0
+            score2 += 0 + 1
         elif p2 == 'Y':
             score += 2 + 3
+            score2 += 3 + 2
         elif p2 == 'Z':
             score += 3 + 6
+            score2 += 6 + 3
     elif p1 == 'C':
         if p2 == 'X':
             score += 1 + 6
+            score2 += 0 + 2
         elif p2 == 'Y':
             score += 2 + 0
+            score2 += 3 + 3
         elif p2 == 'Z':
             score += 3 + 3
+            score2 += 1 + 6
 
 
 print(f"Part 1: {score}")
-
-score = 0
-for j in content:
-    try:
-        p1 = j[0]
-        p2 = j[2]
-    except:
-        continue
-    
-    if p1 == 'A':
-        if p2 == 'X':
-            score += 0 + 3
-        elif p2 == 'Y':
-            score += 3 + 1
-        elif p2 == 'Z':
-            score += 6 + 2
-    elif p1 == 'B':
-        if p2 == 'X':
-            score += 0 + 1
-        elif p2 == 'Y':
-            score += 3 + 2
-        elif p2 == 'Z':
-            score += 6 + 3
-    elif p1 == 'C':
-        if p2 == 'X':
-            score += 0 + 2
-        elif p2 == 'Y':
-            score += 3 + 3
-        elif p2 == 'Z':
-            score += 1 + 6
-
-print(f"Part 2: {score}")
+print(f"Part 2: {score2}")
