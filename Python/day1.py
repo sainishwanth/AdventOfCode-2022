c8721f224fd801d34cd0223b76af2f2280ce21c2
Sai Nishwanth Raj <sainishwanthraj@gmail.com>
2022-12-02 11:07:58 +0530

    Day 1 - Python

---
+++ Python/day1.py
@@ -0,0 +1,18 @@
+with open('day1.txt', "r") as file:
+    content = file.read().split("\n")
+
+max_snacks = 0
+count_sum = 0
+snacks = []
+for i in content:
+    if i == '':
+        max_snacks = count_sum if count_sum > max_snacks else max_snacks
+        snacks.append(int(count_sum))
+        count_sum = 0
+        continue
+    count_sum += int(i)
+
+print(f"Part 1: {max_snacks}")
+
+snacks.sort(reverse=True)
+print(f"Part 2: {snacks[0] + snacks[1] + snacks[2]}")
