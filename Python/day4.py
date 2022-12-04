21ece2d3e5e2c6966b2f6ada9e640d2d5fc7eba1
Sai Nishwanth Raj <sainishwanthraj@gmail.com>
2022-12-04 20:30:58 +0530

    Day 4 - Part 1

--- Python/day4.py
+++ Python/day4.py
@@ -1,25 +1,30 @@
-with open('day4.txt', 'r') as file:
+with open('/Users/sainishwanth/Documents/College/Coding/AdventOfCode2022/Python/day4.txt', 'r') as file:
     content = file.read().splitlines()
 
 str = ''
 
 count = 0
 
-for i in content[0:10]:
+for i in content:
     str = i
     r1, r2 = i.split(",", 1)
     r11, r12 = (r1.split("-", 1))
     r21, r22 = (r2.split("-", 2))
     r11, r12 = int(r11), int(r12)
     r21, r22 = int(r21), int(r22)
-    print(f"elf 1 - {r11}-{r12}")
-    print(f"elf 2 - {r21}-{r22}")
-    xset = set(range(r11,r12+1))
-    yset = set(range(r21,r22+1))
-    x_inter_y = xset.update(yset)
-    print(x_inter_y)
-    if len(x_inter_y) == len(xset):
+    l1 = [j for j in range(r11,r12+1)]
+    l2 = [k for k in range(r21,r22+1)]
+    similar = []
+    for j1 in l1:
+        if j1 in l2:
+            similar.append(j1)
+    if similar == l1:
         count += 1
+        continue
+    elif similar == l2:
+        count += 1
+    
+    
 
 print(count)
 
