070bccfb3fc29d368b39fc09636f2d011c258ca6
Sai Nishwanth Raj <sainishwanthraj@gmail.com>
2022-12-04 20:56:03 +0530

    Day 4 - Python

--- Python/check.py
+++ Python/check.py
@@ -4,10 +4,10 @@ r12 = 5
 r21 = 1
 r22 = 5
 
-l1 = {j for j in range(r11,r12+1)}
-l2 = {k for k in range(r21,r22+1)}
+l1 = [j for j in range(r11,r12+1)]
+l2 = [k for k in range(r21,r22+1)]
 print(l1)
 l1.extend(l2)
 s1 = set(l1)
 s2 = set(l1)
-print(s1.intersection(s2))
+print(len(s1))
--- Python/day4.py
+++ Python/day4.py
@@ -1,10 +1,8 @@
-with open('/Users/sainishwanth/Documents/College/Coding/AdventOfCode2022/Python/day4.txt', 'r') as file:
+import os
+with open('day4.txt', 'r') as file:
     content = file.read().splitlines()
 
-str = ''
-
 count = 0
-
 for i in content:
     str = i
     r1, r2 = i.split(",", 1)
@@ -23,9 +21,25 @@ for i in content:
         continue
     elif similar == l2:
         count += 1
-    
+        
+print(f"Part 1: {count}")
+count2 = 0
+
+for i in content:
+    str = i
+    r1, r2 = i.split(",", 1)
+    r11, r12 = (r1.split("-", 1))
+    r21, r22 = (r2.split("-", 2))
+    r11, r12 = int(r11), int(r12)
+    r21, r22 = int(r21), int(r22)
+    l1 = [j for j in range(r11,r12+1)]
+    l2 = [k for k in range(r21,r22+1)]
+    for j in l1:
+        if j in l2:
+            count2 += 1
+            break
     
 
-print(count)
+print(f"Part 2: {count2}")
 
       
