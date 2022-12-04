e892ddb187253906e3e002c57a7707d13d3ca05b
sainishwanth <sainishwanthraj@gmail.com>
2022-12-04 22:34:37 +0530

    Day 4 Python

--- Python/day4.py
+++ Python/day4.py
@@ -3,6 +3,8 @@ with open('day4.txt', 'r') as file:
     content = file.read().splitlines()
 
 count = 0
+count2 = 0
+check = False
 for i in content:
     str = i
     r1, r2 = i.split(",", 1)
@@ -15,7 +17,11 @@ for i in content:
     similar = []
     for j1 in l1:
         if j1 in l2:
+            check = True
             similar.append(j1)
+    if check:
+        count2 += 1
+        check = False
     if similar == l1:
         count += 1
         continue
@@ -23,23 +29,8 @@ for i in content:
         count += 1
         
 print(f"Part 1: {count}")
-count2 = 0
-
-for i in content:
-    str = i
-    r1, r2 = i.split(",", 1)
-    r11, r12 = (r1.split("-", 1))
-    r21, r22 = (r2.split("-", 2))
-    r11, r12 = int(r11), int(r12)
-    r21, r22 = int(r21), int(r22)
-    l1 = [j for j in range(r11,r12+1)]
-    l2 = [k for k in range(r21,r22+1)]
-    for j in l1:
-        if j in l2:
-            count2 += 1
-            break
-    
-
 print(f"Part 2: {count2}")
 
-      
+   
+
+     
