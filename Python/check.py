be548f5795e6ccaf14d5b68d48ce02a32e9b971d
Sai Nishwanth Raj <sainishwanthraj@gmail.com>
2022-12-05 13:31:13 +0530

    Git_Ignore

--- Python/check.py
+++
@@ -1,8 +0,0 @@
-with open('/Users/sainishwanth/Documents/College/Coding/AdventOfCode2022/Python/day5.txt', 'r') as file:
-    content = file.read().splitlines()
-
-print(int(content[0][17:19].strip()))
-
-s1 = ['P', 'L', 'M', 'N', 'W', 'V', 'B', 'H']
-print(s1.pop())
-print(s1)
